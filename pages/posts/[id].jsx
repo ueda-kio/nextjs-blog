3170059ff8d008a9421d613a04281db56d81f09b
上田貴央 <gibson511511@gmail.com>
2022-03-27 23:37:12 +0900

    react-markdown

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,14 +1,21 @@
+import Head from 'next/head'
 import Layout from '../components/layout';
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import ReactMarkdown from 'react-markdown';
 
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br />
             {postData.id}
             <br />
             {postData.date}
+            <br />
+            <ReactMarkdown children={postData.fileContents} />
         </Layout>
     )
 }
@@ -22,7 +29,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id);
     return {
         props: {
             postData
