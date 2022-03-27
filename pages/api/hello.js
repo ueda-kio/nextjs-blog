59fb44d158bab9f790707b4b8c86827123d4f867
上田貴央 <gibson511511@gmail.com>
2022-03-27 23:56:33 +0900

    api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const handler = (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-};
-
-export default handler;
+export default (req, res) => {
+  res.status(200).json({
+    text: 'Hello'
+  })
+}
