513524f28e331fec669092ad82cf56ca0ef00f12
上田貴央 <gibson511511@gmail.com>
2022-03-27 18:06:26 +0900

    rewrite jsx

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const FirstPost = () => {
+	return (
+		<>
+			<Head>
+				<title>First Post</title>
+			</Head>
+			<h1>First Post</h1>
+		</>
+	)
+};
+
+export default FirstPost;
